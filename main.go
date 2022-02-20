887420bbd3e7016405682b0af6c3c42e6f5e2a48
AdityaVarmaUddaraju <adithya951@gmail.com>
2022-02-20 17:59:15 +0530

    loops in go

--- main.go
+++ main.go
@@ -1,30 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+
+	// for loop
+	for i:=0; i<10; i++ {
+		fmt.Printf("number is %v\n", i)
+	}
+
+	// for each
+	slice := []string{"element1","element2"}
+	// syntax index, element := range list {}
+	for _,element := range slice { 
+		fmt.Printf("element is %v\n", element)
+	}
+
 	// variables in go lang
 	var movieName = "After Hours"
 	const totalTicketsAvailable uint = 50
 	var remainingTickets uint = 50
-
-	// static length arrays
-	var fixedLengthArray [10]string 
-	fixedLengthArray[2] = "element3" // elements can be assigned using index
-	fmt.Printf("Whole array is %v\n",fixedLengthArray)
-	fmt.Printf("Datatype of array is %T\n", fixedLengthArray)
-	fmt.Printf("first element of array %v\n", fixedLengthArray[0])
-	fmt.Printf("length of array is %v\n",len(fixedLengthArray))
-
-	// dynamic length arrays
-	// variable length arrays are called slices in go
-	slices := []string{}
-	slices = append(slices, "element1") // elements can be assigned using append()
-	fmt.Printf("Whole slice is %v\n",slices)
-	fmt.Printf("Datatype of slice is %T\n", slices)
-	fmt.Printf("first element of slice is %v\n", slices[0])
-	fmt.Printf("length of array slice %v\n",len(slices))
-
 
 	// Display Welcome message for the users
 	// We can send data to standard output using fmt.Print func in go lang
@@ -39,20 +37,32 @@ func main() {
 
 	bookings := []string{}
 
-	fmt.Println("Enter your first name")
-	// User input from standard input can be read using fmt.Scan
-	fmt.Scan(&firstName) // '&' indicates reference to the variable firstName
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)	
-	fmt.Println("Enter your email")
-	fmt.Scan(&email)
-	fmt.Println("Enter number of tickets required")
-	fmt.Scan(&ticketsBooked)
+	for {
+		fmt.Println("Enter your first name")
+		// User input from standard input can be read using fmt.Scan
+		fmt.Scan(&firstName) // '&' indicates reference to the variable firstName
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)	
+		fmt.Println("Enter your email")
+		fmt.Scan(&email)
+		fmt.Println("Enter number of tickets required")
+		fmt.Scan(&ticketsBooked)
+	
+		remainingTickets -= ticketsBooked
+		bookings = append(bookings, firstName+" "+lastName)
+	
+		fmt.Printf("Thank you %v, for booking %v tickets. Your tickets will be sent to %v email\n",firstName, ticketsBooked, email)
+		fmt.Printf("Remaining tickets available for %v are %v\n", movieName, remainingTickets)
+	
+		var booking_names []string
+	
+		for _,name := range bookings {
+			full_name := strings.Split(name," ")
+			booking_names = append(booking_names, full_name[0])
+		}
+	
+		fmt.Printf("Current Bookings are %v\n",booking_names)
+	}
 
-	remainingTickets -= ticketsBooked
-	bookings = append(bookings, firstName+" "+lastName)
-
-	fmt.Printf("Thank you %v, for booking %v tickets. Your tickets will be sent to %v email\n",firstName, ticketsBooked, email)
-	fmt.Printf("Remaining tickets available for %v are %v\n", movieName, remainingTickets)
-	fmt.Printf("Current Bookings are %v\n",bookings)
+	
 }
