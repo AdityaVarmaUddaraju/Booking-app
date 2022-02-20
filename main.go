fecfe12925a3635c540dd92dc4f0ab7202a6244a
AdityaVarmaUddaraju <adithya951@gmail.com>
2022-02-20 22:02:12 +0530

    functions in go

--- main.go
+++ main.go
@@ -7,72 +7,37 @@ import (
 
 func main() {
 
-	// switch statements in go
-	theaterName := "After Hours"
-
-	switch theaterName {
-		case "After Hours": {
-			fmt.Println("Movie name is After Hours")
-		}
-		case "Avengers", "Spider-Man": {
-			fmt.Println("Movie name is Avengers or Spider-Man")
-		}
-		default: {
-			fmt.Println("Unknown movie name")
-		}
-
-	}
-
 	// variables in go lang
 	var movieName = "After Hours"
 	const totalTicketsAvailable int = 50
 	var remainingTickets int = 50
 
-	// Display Welcome message for the users
-	// We can send data to standard output using fmt.Print func in go lang
-	fmt.Printf("Welcome to %v Movie Ticket Booking App\n",movieName)
-	fmt.Printf("We have a total of %v tickets and %v are remaining\n", totalTicketsAvailable, remainingTickets)
-	fmt.Println("Please book your desired seats for the film")
-
-	var firstName string
-	var lastName string
-	var email string
-	var ticketsBooked int
+	greetUser(movieName, totalTicketsAvailable, remainingTickets)
 
 	bookings := []string{}
 
 	for {
-		fmt.Println("Enter your first name")
-		// User input from standard input can be read using fmt.Scan
-		fmt.Scan(&firstName) // '&' indicates reference to the variable firstName
-		fmt.Printf("first name is %v\n", firstName)
-		fmt.Println("Enter your last name")
-		fmt.Scan(&lastName)	
-		fmt.Println("Enter your email")
-		fmt.Scan(&email)
-		fmt.Println("Enter number of tickets required")
-		fmt.Scan(&ticketsBooked)
+		firstName, lastName, email, ticketsBooked := getUserInput()
 
-		validName := len(firstName) > 1 && len(lastName) > 1
-		validEmail := strings.Contains(email, "@")
-		validTicketNumber := ticketsBooked > 0
+		validName, validEmail, validTicketNumber := validateUser(firstName, lastName, email, ticketsBooked)
+
 		skip := false
 
 		if validName && validEmail && validTicketNumber {
 
-			ticketsAvailable := remainingTickets >= ticketsBooked
+			ticketsAvailable := checkTicketAvailability(remainingTickets, ticketsBooked)
 
 			for !ticketsAvailable{
 				fmt.Printf("We have only %v tickets available\n", remainingTickets)
 				fmt.Println("Enter Number of tickets required")
 				fmt.Scan(&ticketsBooked)
-				validTicketNumber = ticketsBooked > 0
+				validTicketNumber = validateTickets(ticketsBooked)
 				if !validTicketNumber {
 					fmt.Println("You have entered invalid ticket number")
 					skip = true
 					break
 				}
-				ticketsAvailable = remainingTickets >= ticketsBooked
+				ticketsAvailable = checkTicketAvailability(remainingTickets, ticketsBooked)
 			}
 
 			if skip {
@@ -86,14 +51,7 @@ func main() {
 			fmt.Printf("Thank you %v, for booking %v tickets. Your tickets will be sent to %v email\n",firstName, ticketsBooked, email)
 			fmt.Printf("Remaining tickets available for %v are %v\n", movieName, remainingTickets)
 		
-			var booking_names []string
-		
-			for _,name := range bookings {
-				full_name := strings.Split(name," ")
-				booking_names = append(booking_names, full_name[0])
-			}
-		
-			fmt.Printf("Current Bookings are %v\n",booking_names)
+			printBookingNames(bookings)
 
 			if remainingTickets == 0 {
 				fmt.Println("We are sold out")
@@ -113,7 +71,69 @@ func main() {
 		}
 	
 
+	}	
+}
+
+// func definition in go
+func greetUser(movieName string, totalTicketsAvailable int, remainingTickets int) {
+	// Display Welcome message for the users
+	// We can send data to standard output using fmt.Print func in go lang
+	fmt.Printf("Welcome to %v Movie Ticket Booking App\n",movieName)
+	fmt.Printf("We have a total of %v tickets and %v are remaining\n", totalTicketsAvailable, remainingTickets)
+	fmt.Println("Please book your desired seats for the film")
+}
+
+// functions in go can return multiple outputs
+func getUserInput() (string, string, string, int) {
+	var firstName string
+	var lastName string
+	var email string
+	var ticketsBooked int
+
+	fmt.Println("Enter your first name")
+	// User input from standard input can be read using fmt.Scan
+	fmt.Scan(&firstName) // '&' indicates reference to the variable firstName
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)	
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets required")
+	fmt.Scan(&ticketsBooked)
+
+	return firstName, lastName, email, ticketsBooked
+}
+
+func validateName(fname string, lname string) bool {
+	return len(fname) > 1 && len(lname) > 1
+}
+
+func validateEmail(email string) bool {
+	return strings.Contains(email, "@")
+}
+
+func validateTickets(tickets int) bool {
+	return tickets > 0
+}
+
+func validateUser(fname string, lname string, email string, tickets int) (bool, bool, bool) {
+	validName := validateName(fname, lname)
+	validEmail := validateEmail(email)
+	validTicketNumber := validateTickets(tickets)
+
+	return validName, validEmail, validTicketNumber
+}
+
+func checkTicketAvailability(remainingTickets int, ticketsBooked int) bool {
+	return remainingTickets >= ticketsBooked
+}
+
+func printBookingNames(bookings []string) {
+	var booking_names []string
+		
+	for _,name := range bookings {
+		full_name := strings.Split(name," ")
+		booking_names = append(booking_names, full_name[0])
 	}
 
-	
+	fmt.Printf("Current Bookings are %v\n",booking_names)
 }
