51c9abfa07e7be58f9ff0d79c00424e4d2a419c7
AdityaVarmaUddaraju <adithya951@gmail.com>
2022-02-20 17:03:26 +0530

    covered standard input in go lang

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	// variables in go lang
 	var movieName = "After Hours"
-	const totalTicketsAvailable = 50
-	var remainingTickets = 50
+	const totalTicketsAvailable uint = 50
+	var remainingTickets uint = 50
 
 	// Display Welcome message for the users
 	// We can send data to standard output using fmt.Print func in go lang
@@ -14,28 +14,23 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are remaining\n", totalTicketsAvailable, remainingTickets)
 	fmt.Println("Please book your desired seats for the film")
 
-	// Using datatypes in go
-	var userName string = "Roark"
-	var userTickets int = 1
+	var firstName string
+	var lastName string
+	var email string
+	var ticketsBooked uint
 
-	fmt.Printf("User %v bought %v tickets for the film\n", userName, userTickets)
+	fmt.Println("Enter your first name")
+	// User input from standard input can be read using fmt.Scan
+	fmt.Scan(&firstName) // '&' indicates reference to the variable firstName
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)	
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets required")
+	fmt.Scan(&ticketsBooked)
 
-	// Find the datatype of a variable
-	fmt.Printf("Data type of variable userName is %T and userTickets is %T\n", userName, userTickets)
+	remainingTickets -= ticketsBooked
 
-	// Different datatype and their limitations
-	var unsignedInteger uint8 = 12
-	var signedInteger16 int16 = 25
-	var signedInteger8 int8 = -100
-
-	// Go lang throws an error for mismatched type operations
-	var unsignedResult  = (signedInteger16) * int16(signedInteger8)
-
-	fmt.Printf("Result is %v\n",unsignedResult)
-	fmt.Printf("unsigneg integer type is %T\n", unsignedInteger)
-
-	//syntax sugar
-	variable:=12 // this syntax cannot be used to declare constants
-	fmt.Printf("type of variable is %T and value is %v\n",variable, variable)
-
+	fmt.Printf("Thank you %v, for booking %v tickets. Your tickets will be sent to %v email\n",firstName, ticketsBooked, email)
+	fmt.Printf("Remaining tickets available for %v are %v\n", movieName, remainingTickets)
 }
